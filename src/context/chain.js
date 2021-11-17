34e98005e21ae536c5ded9364fca47da045c6308
Milan Steiner <steiner@travix-media.de>
2021-11-17 18:55:06 +0100

    Fix Typo

--- src/context/chain.js
+++ src/context/chain.js
@@ -1,7 +1,7 @@
 export const chain = {
     "name": "Juno Uni",
     "chain_id": "uni",
-    "lcd": " https://api.uni.junomint.com:443/",
+    "lcd": "https://api.uni.junomint.com:443/",
     "rpc": "https://rpc.uni.junomint.com:443/",
     "coinDenom": "JUNOX",
     "coinMinimalDenom": "ujunox",
