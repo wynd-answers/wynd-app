890f930b1f55905afdb8404426a69525868464a0
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-11-17 18:09:50 +0100

    Use UNI testnet with Keplr

--- src/context/chain.js
+++ src/context/chain.js
@@ -1,12 +1,12 @@
 export const chain = {
-    "name": "Juno",
-    "chain_id": "juno-1",
-    "lcd": "https://lcd-juno.itastakers.com",
-    "rpc": "https://rpc-juno.itastakers.com",
-    "coinDenom": "JUNO",
-    "coinMinimalDenom": "ujuno",
+    "name": "Juno Uni",
+    "chain_id": "uni",
+    "lcd": "http://uni.junochain.com:1317",
+    "rpc": "http://uni.junochain.com:26657",
+    "coinDenom": "JUNOX",
+    "coinMinimalDenom": "ujunox",
     "prefix": "juno",
     "version": "stargate",
     "decimals": 6,
-    "group": "mainnet"
+    "group": "testnet"
 }
