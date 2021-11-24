b359bc976e47dbf366054f6a7d8770e454095bef
Milan Steiner <steiner@travix-media.de>
2021-11-24 15:03:12 +0100

    Disabling Mobile Devices

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import { Container, CssBaseline } from "@mui/material";
+import { CssBaseline, Container, Alert } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Footer from "./footer";
 import Store from "../context/store";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -21,14 +22,27 @@ const theme = createTheme({
     },
 });
 
+
 const Layout = ({ children }) => {
+
+    // Resolution needs to be desktop size
+    const matches = useMediaQuery(theme.breakpoints.up("lg"));
+
     return (
         <Store>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <GlobalStyles styles={{ body: { paddingTop: 50 } }} />
-                {children}
-                <Footer />
+                {matches ?
+                    <>
+                        {children}
+                        <Footer />
+                    </>
+                    :
+                    <Container>
+                        <Alert severity="error">The App isn't optimized on your screen resolution yet. Please try the demo on another device!</Alert>
+                    </Container>
+                }
             </ThemeProvider>
         </Store>
     );
