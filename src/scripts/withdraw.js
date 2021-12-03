f3032bdbb006870818eb674d8de0108a77e3950b
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-12-03 12:28:12 +0100

    Withdraw script works

--- src/scripts/withdraw.js
+++ src/scripts/withdraw.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*jshint esversion: 8 */
 
-// Usage: ./invest.js 8328aefffffffff
+// Usage: ./withdraw.js
 
 /* eslint-disable @typescript-eslint/naming-convention */
 const axios = require("axios");
@@ -21,29 +21,7 @@ const config = {
 const investAddr = "juno12pdkmn8qf09rn5yuf6lpreml8ypf45uzkvwyeztaqpjncpfwk0kqp3mrpr";
 const wyndAddress = "juno1wjur4gvzn0ccnffyuhvs3qxgsxn6ga86wpd2y8s2ufck4c2zmrfsyn44rq";
 
-function logToEvents(logs) {
-    let events = logs[0].events.map(eventObject);
-    return events.reduce((obj, evt) => Object.assign(obj, evt), {});
-}
-
-function eventObject(event) {
-    let attributes = event.attributes.reduce((obj, attr) => Object.assign(obj, { [attr.key]: attr.value }), {});
-    return { [event.type]: attributes };
-}
-
-// this turns a JSON object to a base64-encoding of the serialized JSON
-// this is needed when sending cw20 tokens to a contracts (see "send" below)
-function encodeMsg(obj) {
-  return toBase64(toAscii(JSON.stringify(obj)));
-}
-
 async function main() {
-    if (process.argv.length != 3) {
-      console.error("Usage: ./invest.js <r3 index>");
-      process.exit(1);
-    }
-    const hex = process.argv[2];
-
     // use the faucet account to upload (it has fee tokens)
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
       prefix: config.bech32prefix,
@@ -60,38 +38,28 @@ async function main() {
       options
     );
 
-    let juno = await client.getBalance(address, "ujunox");
-    console.info(`Balance: ${juno.amount} ${juno.denom}`);
-
     let { balance } = await client.queryContractSmart(wyndAddress, {balance: {address}});
     console.info(`Balance: ${balance} uWYND`);
 
-    let { investments } = await client.queryContractSmart(investAddr, {list_investments: {investor: address}});
-    console.info(`All investments:`);
-    investments.forEach(x => console.log(x));
-
     const fee = {
-      gas: "400000",
+      gas: "800000",
       amount: [{
         denom: config.feeDenom,
         // 0.025 * gas
-        amount: "10000",
+        amount: "20000",
       }]
     };
 
-    const invest = { invest: { hex }};
-    const send = { send: {
-      contract: investAddr,
-      amount: "123000000", // 123 WYND
-      msg: encodeMsg(invest),
-    }}
-
+    const msg = { withdraw: { }};
     console.log("Executing...");
-    const { logs } = await client.execute(address, wyndAddress, send, fee, "Make Investment");
+    const { logs } = await client.execute(address, investAddr, msg, fee, "Withdraw Investments");
+
+    let { balance: newBalance } = await client.queryContractSmart(wyndAddress, {balance: {address}});
+    console.info(`New Balance: ${newBalance} uWYND`);
 
     console.debug(`Execute succeeded. Receipt: `);
-    const events = logToEvents(logs);
-    console.log(events);
+    const events = logs[0].events;
+    console.log(JSON.stringify(events, undefined, 2));
   }
   
   main().then(
