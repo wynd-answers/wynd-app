58ab1a31e1b52e2e0a7e06858fa284e51e52b1c0
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-12-04 19:37:03 +0100

    Read mnemonics from env variable

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,10 +1,18 @@
+const mnemonic = process.env["MNEMONIC"];
+if (!mnemonic) {
+    console.error("You must set MNEMONIC to run the script")
+    process.exit(1);
+}
+
+// ex for invest use
+//     'wild enact trust mean try snack evoke bring gown core curtain ahead',
+
 
 const config = {
     endpoint: 'https://rpc.uni.junomint.com:443/',
     bech32prefix: 'juno',
     feeDenom: 'ujunox',
-    mnemonic:
-    'wild enact trust mean try snack evoke bring gown core curtain ahead',
+    mnemonic,
 };
 
 const contracts = {
