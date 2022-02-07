7b72bab29a00d1103df5a53afbb5c1823bb4007f
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-02-07 15:23:36 +0100

    Update to uni-2 (#30)
    * Use new chain id in keplr config
    
    * Re-upload everything and adjust addresses
    
    * Update contract addresses for app

--- src/scripts/query_wynd.js
+++ src/scripts/query_wynd.js
@@ -4,6 +4,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 const { CosmWasmClient } = require('@cosmjs/cosmwasm-stargate');
 
+const { contracts } = require("./config");
+
 // from ../context/chain.js
 const config = {
     endpoint: 'https://rpc.uni.wyndex.io:443/',
@@ -13,25 +15,23 @@ const config = {
       'wild enact trust mean try snack evoke bring gown core curtain ahead',
 };
   
-const wyndAddress = "juno1wjur4gvzn0ccnffyuhvs3qxgsxn6ga86wpd2y8s2ufck4c2zmrfsyn44rq";
-
 async function main() {
     const client = await CosmWasmClient.connect(config.endpoint);
 
     const toCheck = [
         // main accounts
-        "juno1pxa9trxza5e2w7sdk2a0xng86y4fnena4e0pka",
-        "juno1qd3p79cd82hz8p20gr4gcqze33eauuka6929w0",
-        // secondary unloaded account
-        "juno19u5ugg4r3haggcstnk7vccxfyju3n56hf23r07",
+        "juno1v2gdcsw27ncrrch3e78c5tfym4su8zlwv2r9rd",
+        "juno14zw7q2rhk76uzvpj3a9yw88zjp0qgaa79zf9dv",
+        "juno1l3493que6nqetkf73qc2mfknd5jn5mlc7qw8wv",
         // faucet itself
-        "juno1w6tvhn4gsp5wxfzqr08rgvfe29zx06rq92nep5j8scvv5dfl79ws72t4uw"
+        "juno1kmya9p26a9ecq07sxdgkkfmr8pcqyszv7gmkyqz6hlt006qr75lq2a8mw9"
     ];
 
     for (const address of toCheck) {
+        const { amount} = await client.getBalance(address, "ujunox");
         const query = {balance: {address}};
-        const {balance} = await client.queryContractSmart(wyndAddress, query);
-        console.log(`${address}: ${balance}`);
+        const {balance} = await client.queryContractSmart(contracts.wyndAddr, query);
+        console.log(`${address}: ${amount} ujunox / ${balance} uWYND`);
     }
 
 }  
